ce6884910e421cee13681df8753b5a95ca7e5032
Renata Carreira <re.carreira.11@gmail.com>
2013-03-05 14:41:09 -0300

    foy_python/python_parser.py: fix ident

--- foy_python/python_parser.py
+++ foy_python/python_parser.py
@@ -3,18 +3,37 @@ import re
 import codecs
 
 def requirements_parser(file_path):
-	string = get_string_from_file(file_path)
-	get_packages_and_versions(string)
+    string = get_string_from_file(file_path)
+    get_packages_and_versions(string)
 
 def get_string_from_file(file_path):
-	input = codecs.open(file_path, mode="r", encoding="utf-8")
-	string = input.read()
-	return string
+    input = codecs.open(file_path, mode="r", encoding="utf-8")
+    string = input.read()
+    return string
+
+def get_operator(requirement):
+    equal = re.compile(".*==.*")
+    equal_major = re.compile(".*>=.*")
+    equal_minor = re.compile(".*<=.*")
+    major = re.compile(".*>.*")
+    minor = re.compile(".*<.*")
+    op = ""
+    if equal.match(requirement):
+        op = "=="
+    elif equal_major.match(requirement):
+        op = ">="
+    elif equal_minor.match(requirement):
+        op = "<="
+    elif major.match(requirement):
+        op = ">"
+    elif minor.match(requirement):
+        op = "<"
+    return op
 
 def get_packages_and_versions(string):
-	regex = re.compile("^[^#].*==.*$",re.MULTILINE)
-	requirements_list = regex.findall(string)
-	requirements = {}
-	for requirement in requirements_list:
-		package, version = requirement.split('==')
-		print "{package} {version}".format(package=package, version=version)
+    regex = re.compile("^[^#].*[==,>,>=]\S*",re.MULTILINE)
+    requirements_list = regex.findall(string)
+    for requirement in requirements_list:
+        op = get_operator(requirement)
+        package, version = requirement.split(op)
+        print "{package} {version}".format(package=package, version=version)
